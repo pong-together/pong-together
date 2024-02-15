5defb48c059718e613a9bfa6adeefc844a6236f8
vlssm <chrrvlssm@gmail.com>
2024-02-15 20:51:20 +0900

    feat/pong-together/frontend#8: 원격 레이아웃 추가

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,13 +1,14 @@
 import Login from './components/Login.js';
 import GameSelect from './components/GameSelect.js';
 import Tournament from './components/Tournament.js';
+import Remote from './components/Remote.js';
 
 export default (main) => {
 	const login = () => new Login(main);
 	const gameSelect = () => new GameSelect(main);
 	const local = () => null;
 	const tournament = () => new Tournament(main);
-	const remote = () => null;
+	const remote = () => new Remote(main);
 	const game = () => null;
 
 	return {
