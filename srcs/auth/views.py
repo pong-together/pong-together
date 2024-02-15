9561a402fbe8adeffc7c8ac73ebaa67d4616d221
yeeuniii <anna862700@gmail.com>
2024-02-16 00:09:23 +0900

    feat/pong-together/backend#5: 42API 호출

--- srcs/auth/views.py
+++ srcs/auth/views.py
@@ -3,16 +3,19 @@ import os
 import requests
 
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from users.models import User
 
 # Create your views here.
 
 REDIRECT_URI = os.environ.get('REDIRECT_URI')
 STATE = os.environ.get('STATE')
 TOKEN_URI = 'https://api.intra.42.fr/oauth/token'
+API_URI = 'https://api.intra.42.fr/v2/me'
 
 
 class LoginView(APIView):
@@ -24,11 +27,33 @@ class LoginView(APIView):
         code = request.data.get('code')
         try:
             if code is None:
-                raise status.HTTP_400_BAD_REQUEST
+                raise ValidationError()
             access_token = self.get_access_token(code)
-        except status:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        return Response(access_token, status=status.HTTP_200_OK)
+
+            header = {'Authorization': f'Bearer {access_token}'}
+            api_response = requests.get(API_URI, headers=header)
+            if api_response.status_code != status.HTTP_200_OK:
+                raise ValidationError()
+            api_response = api_response.json()
+            intra_id = api_response.get('login')
+            image_url = api_response.get('image').get('versions').get('medium')
+            if intra_id is None or image_url is None:
+                raise ValidationError()
+
+            try:
+                user = User.objects.get(intra_id=intra_id)
+            except User.DoesNotExist:
+                user = User.objects.create_user(intra_id, image_url)
+
+            data = {
+                "id": user.id
+            }
+
+        except ValidationError:
+            data = {'login': 'fail'}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(data=data, status=status.HTTP_200_OK)
 
     def get_access_token(self, code):
         body = {
@@ -39,9 +64,10 @@ class LoginView(APIView):
             'redirect_uri': REDIRECT_URI,
             'state': STATE
         }
-        token_response = requests.post(TOKEN_URI, data=body).json()
-        error = token_response.get('error')
-        access_token = token_response.get('access_token')
-        if error is not None or access_token is None:
-            raise status.HTTP_400_BAD_REQUEST
+        token_response = requests.post(TOKEN_URI, data=body)
+        if token_response.status_code != status.HTTP_200_OK:
+            raise ValidationError()
+        token_response = token_response.json()
+        error = token_response['error']
+        access_token = token_response['access_token']
         return access_token
