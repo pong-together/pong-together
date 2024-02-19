479982c74e5f717a26de2a1ecee58fdc9cda9183
ppparkta <pppppark2@gmail.com>
2024-02-19 19:57:22 +0900

    feat/pong-together/forntend#12: localStorage에 accessToken 저장

--- src/pages/components/login/Login.js
+++ src/pages/components/login/Login.js
@@ -33,6 +33,7 @@ export default class extends Component {
 				);
 				console.log('data', data);
 				if (data.login === 'success') {
+					localStorage.setItem('accessToken', data.access_token);
 					this.$store.dispatch('isLogin', true);
 				} else if (data.login === 'fail') {
 					this.$store.dispatch('isTwoFA', true);
@@ -46,8 +47,10 @@ export default class extends Component {
 	async mounted() {
 		const $parent = this.$target.querySelector('.login-content-wrapper');
 		if (this.$state.progress === 'oauth') {
-			new OauthBtn($parent, this.$store);
-			this.oauth();
+			if (localStorage.getItem('accessToken') === undefined) {
+				new OauthBtn($parent, this.$store);
+				this.oauth();
+			}
 		} else if (this.$state.progress === 'twoFA') {
 			new TFABtn($parent, this.$store);
 		} else if (this.$state.progress === 'language') {
