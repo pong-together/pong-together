fbbd932b8a9f2bfb4d7ca0e421a50f5e3bf37503
jisunggi <wltjd5316@naver.com>
2024-02-19 15:18:43 +0900

    feat/pong-together/backend#9: 토너먼트 랜덤 매칭 기능 구현

--- srcs/tournaments/serializers.py
+++ srcs/tournaments/serializers.py
@@ -1,3 +1,5 @@
+import random
+
 from rest_framework import serializers
 
 from tournaments.models import Tournament
@@ -7,6 +9,21 @@ class TournamentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tournament
         fields = ('player1_name', 'player2_name', 'player3_name', 'player4_name')
+
+    def create(self, validated_data):
+
+        player_names = list(validated_data.values())
+
+        shuffled_player_names = random.sample(player_names, len(player_names))
+
+        tournament = Tournament(
+            player1_name=shuffled_player_names[0],
+            player2_name=shuffled_player_names[1],
+            player3_name=shuffled_player_names[2],
+            player4_name=shuffled_player_names[3],
+        )
+        tournament.save()
+        return tournament
 
 
 class TournamentSerializer(serializers.ModelSerializer):
