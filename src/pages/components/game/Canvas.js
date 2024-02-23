31308b1528eb0c0e7b294ee89141947d2af1d5b1
vlssm <chrrvlssm@gmail.com>
2024-02-23 17:53:01 +0900

    fix/pong-together/frontend#37: Game Canvas 이미지 및 키 입력 작업

--- src/pages/components/game/Canvas.js
+++ src/pages/components/game/Canvas.js
@@ -16,10 +16,10 @@ export default class extends Component {
 		const ctx = canvas.getContext('2d');
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
+		
 		let img_p1 = new Image();
 		let img_p2 = new Image();
 		let img_ball = new Image();
-		let sign = false;
 
 		img_p1.src = '../../../../static/images/player1_bar.png'
 		img_p2.src = '../../../../static/images/player2_bar.png'
@@ -35,8 +35,8 @@ export default class extends Component {
 			}
 			
 			draw() {
-				ctx.fillStyle = 'black';
-				ctx.fillRect(this.x, this.y, this.width, this.height);
+				// ctx.fillStyle = 'black';
+				// ctx.fillRect(this.x, this.y, this.width, this.height);
 				ctx.drawImage(this.image, this.x, this.y);
 			}
 		}
@@ -50,8 +50,8 @@ export default class extends Component {
 			}
 
 			draw() {
-				ctx.fillStyle = 'blue';
-				ctx.fillRect(this.x, this.y, this.width, this.height);
+				// ctx.fillStyle = 'blue';
+				// ctx.fillRect(this.x, this.y, this.width, this.height);
 				ctx.drawImage(img_ball, this.x, this.y);
 			}
 		}
@@ -60,6 +60,9 @@ export default class extends Component {
 		let player2 = new bar(927, 625, 18, 62, img_p2);
 		let ball = new sphere(630, 640, 30, 30);
 
+		// let player1 = new bar(414, 624, 18, 62, img_p1);
+		// let player2 = new bar(1007, 624, 18, 62, img_p2);
+		// let ball = new sphere(630, 640, 30, 30);
 	
 
 		function frame() {
@@ -82,26 +85,16 @@ export default class extends Component {
 						break;
 				}
 			}
-
-			// if (sign === true) {
-			// 	console.log('test2');
-			// 	player2.y -= 50;
-			// 	sign = false;
-			// }
 			
 			document.addEventListener('keydown', e => {
 				move(e.code);
-				
-				// if (e.code === 'KeyW') {
-				// 	sign = true;
-				// }
 			});
 
 			player1.draw();
 			player2.draw();
 			ball.draw();
 		}
-
+		
 		frame();
 	}
 
