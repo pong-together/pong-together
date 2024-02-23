a4b7ec62ea653df2c68485f7bd262aeee90cff9e
ppparkta <pppppark2@gmail.com>
2024-02-23 17:37:34 +0900

    fix#38

--- src/pages/components/login/Login.js
+++ src/pages/components/login/Login.js
@@ -23,6 +23,7 @@ export default class extends Component {
 		const $parent = this.$target.querySelector('.login-body-wrapper');
 
 		if (store.state.loginProgress === 'done') {
+			window.location.pathname = '/';
 			window.location.hash = '#/select';
 		}
 		if (store.state.loginProgress === 'oauth') {
