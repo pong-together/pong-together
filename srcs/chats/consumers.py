66e9e95e77336181dcf6868d71b61c6d752a8352
yeeuniii <anna862700@gmail.com>
2024-02-25 02:29:13 +0900

    refactor/pong-together/backend#24: group name 변수 클래스 상수로 변경

--- srcs/chats/consumers.py
+++ srcs/chats/consumers.py
@@ -1,18 +1,18 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-GROUP_NAME = 'pong-together'
-
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
+    GROUP_NAME = 'pong-together'
+
     async def connect(self):
         try:
-            await self.channel_layer.group_add(GROUP_NAME, self.channel_name)
+            await self.channel_layer.group_add(self.GROUP_NAME, self.channel_name)
             await self.accept()
         except Exception:
             await self.close()
 
     async def disconnect(self, code):
         try:
-            await self.channel_layer.group_discard(GROUP_NAME, self.channel_name)
+            await self.channel_layer.group_discard(self.GROUP_NAME, self.channel_name)
         except Exception as e:
             await self.send_json({'error': str(e)})
