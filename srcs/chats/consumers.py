5d0112fdc5291de117afacaf1f5f8fb32ad74f03
yeeuniii <anna862700@gmail.com>
2024-02-25 02:28:19 +0900

    feat/pong-together/backend#24: websocket 연결해제 구현

--- srcs/chats/consumers.py
+++ srcs/chats/consumers.py
@@ -8,5 +8,11 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         try:
             await self.channel_layer.group_add(GROUP_NAME, self.channel_name)
             await self.accept()
-        except Exception as e:
+        except Exception:
             await self.close()
+
+    async def disconnect(self, code):
+        try:
+            await self.channel_layer.group_discard(GROUP_NAME, self.channel_name)
+        except Exception as e:
+            await self.send_json({'error': str(e)})
