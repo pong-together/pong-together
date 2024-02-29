3eb59a2ce6cde4e7625d1458457f2ed1ba746d7a
yeeuniii <anna862700@gmail.com>
2024-02-29 17:27:04 +0900

    fix/pong-together/backend#38: settings 환경변수 불러오기 이슈 해결

--- srcs/pong_together/settings.py
+++ srcs/pong_together/settings.py
@@ -14,8 +14,15 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# get .env
+
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
