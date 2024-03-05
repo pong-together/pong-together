e2df1efe1631153c12a8477e414f00b29b561cff
yeeuniii <anna862700@gmail.com>
2024-03-05 15:31:08 +0900

    refactor/pong-together/backend#44: 게임 디비 제약조건 추가

--- srcs/games/models.py
+++ srcs/games/models.py
@@ -8,10 +8,10 @@ class Game(models.Model):
     player2_name = models.CharField()
     player1_score = models.IntegerField(default=0)
     player2_score = models.IntegerField(default=0)
-    player1_y = models.IntegerField()
-    player2_y = models.IntegerField()
-    ball_x = models.IntegerField()
-    ball_y = models.IntegerField()
+    player1_y = models.IntegerField(null=True)
+    player2_y = models.IntegerField(null=True)
+    ball_x = models.IntegerField(null=True)
+    ball_y = models.IntegerField(null=True)
 
     class DoesNotExist(Exception):
         def __init__(self):
