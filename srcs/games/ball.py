9511010a018193eed910d89ca0db91a3961a18a6
yeeuniii <anna862700@gmail.com>
2024-03-05 20:36:13 +0900

    refactor/pong-together/backend#33: ball 로직 리팩터링

--- srcs/games/ball.py
+++ srcs/games/ball.py
@@ -1,64 +1,75 @@
 import math
+from random import randint
 
 
 class Ball:
     WIDTH = 20
     HEIGHT = 20
-    MINIMUM_SPEED = 7
+    RESET_SPEED = 5
+    MAXIMUM_SLOPE = 1
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.velocity = [7, 0]
+        self.velocity = [5, 0]
 
     def set_position(self, x, y):
         self.x = x
         self.y = y
 
-    def update(self, game, turn):
-        self.update_position()
-        return self.update_velocity(game, turn)
+    def reset(self):
+        from games.pong import Pong
+
+        x = (Pong.WIDTH - self.WIDTH) / 2
+        y = (Pong.HEIGHT - self.HEIGHT) / 2 + randint(-100, 100)
+        self.set_position(x, y)
 
     def update_position(self):
         self.x += self.velocity[0]
         self.y += self.velocity[1]
 
-    def update_velocity(self, game, turn):
-        top_limit = 0
+    def update_velocity(self, turn):
+        self.update_vertical_velocity()
+        return self.update_horizontal_velocity(turn)
+
+    def update_vertical_velocity(self):
         from games.pong import Pong
+
+        top_limit = 0
         bottom_limit = Pong.HEIGHT - self.HEIGHT
         if self.y <= top_limit:
             self.velocity[1] *= -1
         if self.y >= bottom_limit:
             self.velocity[1] *= -1
 
+    def update_horizontal_velocity(self, turn):
+        from games.pong import Score, Pong
+
         escape_degree = 15
         left_limit = -(self.WIDTH + escape_degree)
         right_limit = Pong.WIDTH + escape_degree
-        from games.pong import Score
         if self.x < left_limit:
-            self.go_off_screen(game, (turn == 1) - (turn == 0))
+            self.go_off_screen((turn == 1) - (turn == 0))
             return Score.PLAYER1
         if self.x > right_limit:
-            self.go_off_screen(game, (turn == 1) - (turn == 0))
+            self.go_off_screen((turn == 1) - (turn == 0))
             return Score.PLAYER2
         return Score.NONE
 
-    def go_off_screen(self, game, sign):
-        self.velocity = [game.player1_y % 7, game.player2_y % 7]
+    def go_off_screen(self, sign):
+        self.velocity = [randint(3, 5), randint(0, 4)]
         self.velocity[0] *= sign
-        if self.velocity[0] == 0:
-            self.velocity[0] = 1
         self.adjust_slope()
         self.adjust_speed()
+        self.reset()
 
     def adjust_slope(self):
         slope = self.velocity[1] / self.velocity[0]
-        if abs(slope) > 1:
+        if abs(slope) > self.MAXIMUM_SLOPE:
             self.velocity[1] = 0
 
-    def adjust_speed(self, speed=MINIMUM_SPEED):
+    def adjust_speed(self, speed=RESET_SPEED):
         size = math.sqrt(self.velocity[0] ** 2 + self.velocity[1] ** 2)
-        if size < speed:
+        if speed == self.RESET_SPEED or size < speed:
             rate = speed / size
             self.velocity = [self.velocity[0] * rate, self.velocity[1] * rate]
