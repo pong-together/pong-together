0423e70270c0c593463ec55d12c97b65a078b535
yeeuniii <anna862700@gmail.com>
2024-03-06 10:42:23 +0900

    feat/pong-together/backend#33: 패들 위아래로 움직이기 함수 구현

--- srcs/games/paddle.py
+++ srcs/games/paddle.py
@@ -1,6 +1,7 @@
 class Paddle:
     WIDTH = 19
     HEIGHT = 63
+    MOVEMENT = 10
 
     def __init__(self, x, y):
         self.x = x
@@ -8,3 +9,15 @@ class Paddle:
 
     def set_position(self, y):
         self.y = y
+
+    def move_up(self, movement=MOVEMENT):
+        self.y += movement
+        if self.y < 0:
+            self.set_position(0)
+
+    def move_down(self, movement=MOVEMENT):
+        from games.pong import Pong
+
+        self.y -= movement
+        if self.y > Pong.HEIGHT - self.HEIGHT:
+            self.set_position(Pong.HEIGHT - self.HEIGHT)
