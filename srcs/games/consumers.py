8f5ba70bcaedeca1ef3037862821143f6f4708c7
yeeuniii <anna862700@gmail.com>
2024-03-09 00:40:22 +0900

    feat/pong-together/backend#32: 게임 웹소켓 disconnect 구현

--- srcs/games/consumers.py
+++ srcs/games/consumers.py
@@ -13,6 +13,11 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         super().__init__(args, kwargs)
         self.pong_task = None
         self.pong = None
+        self.type = None
+        self.user = None
+        self.group_name = None
+        self.player1_name = None
+        self.player2_name = None
 
     async def connect(self):
         try:
@@ -26,25 +31,56 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         try:
             if self.type == 'remote':
                 await self.disconnect_remote()
-
-            #토너먼트, 로컬
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
-            await self.cancel_task()
+            await self.cancel_pong_task()
         except Exception as e:
             await self.send_json({'error': str(e)})
 
     async def disconnect_remote(self):
-        if self.pong.winner is None:  # 비정상 종료 처리
-            await self.update_lose()
-            await self.send_end_abnormal()
-            if self.task is None:
-                await self.update_win()
+        if self.is_abnormal():  # 비정상 종료 처리
+            await self.disconnect_abnormal()
         else:  # 정상 종료 처리
-            if self.task:
-                await self.update_result(self.pong.winner)
+            await self.disconnect_normal()
 
         if self.user in self.remote_game[self.group_name]:
             self.remote_game[self.group_name].remove(self.user)
+
+    async def is_abnormal(self):
+        return self.pong.winner is None
+
+    async def disconnect_normal(self):
+        if self.user.intra_id == self.pong.winner:
+            await self.update_win()
+        else:
+            await self.update_lose()
+
+    async def disconnect_abnormal(self):
+        await self.update_lose()
+        self.pong.winner = self.get_other_player()
+        await self.send_end()
+
+    def get_other_player(self):
+        other = self.player1_name
+        if self.user.intra_id == other:
+            other = self.player2_name
+        return other
+
+    async def send_end(self):
+        await self.channel_layer.group_send(self.group_name, {
+            'type': 'end',
+            'is_normal': False,
+            'winner': self.user.intra_id
+        })
+
+    async def end(self, event):
+        await self.send_json(event)
+
+    async def cancel_pong_task(self):
+        self.pong_task.cancel()
+        try:
+            await self.pong_task
+        except asyncio.CancelledError:
+            pass
 
     @database_sync_to_async
     def update_lose(self):
@@ -57,25 +93,6 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         self.user.win_count += 1
         self.user.game_count += 1
         self.user.save()
-
-    async def send_end_abnormal(self):
-        await self.channel_layer.group_send(self.group_name, {
-            'type': 'end_abnormal',
-            'winner': self.user.intra_id
-        })
-
-    async def end_abnormal(self, event):
-        await self.send_json(event)
-
-    async def cancel_task(self):
-        self.task.cancel()
-        try:
-            await self.task
-        except asyncio.CancelledError:
-            pass
-
-    def is_player1(self):
-        return self.user.intra_id == self.player1_name
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
         try:
@@ -101,4 +118,3 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         if sender_player == 'player2':
             player = self.pong.player2
         player.move(button)
-
