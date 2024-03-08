0ef3faf7658504500bc0411b81989c10e75f08cc
yeeuniii <anna862700@gmail.com>
2024-03-08 22:38:51 +0900

    refactor/pong-together/backend#32: 게임 소켓 connect 부분 리팩터링

--- srcs/games/consumers.py
+++ srcs/games/consumers.py
@@ -5,7 +5,6 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from rest_framework.generics import get_object_or_404
 
-from games.models import Game
 from local.models import Local
 from remote.models import Remote
 from tournaments.models import Tournament
@@ -27,48 +26,99 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         self.player2_name = None
         self.pong = None
 
-
     async def connect(self):
         try:
             await self.init_connection()
-
             await self.channel_layer.group_add(self.group_name, self.channel_name)
             await self.accept()
 
             if self.type == 'remote':
-                await self.remote_game_start()
+                await self.start_remote_game()
             else:
-                await self.get_player_user()
+                await self.set_players_name()
                 await self.channel_layer.group_send(self.group_name, {
                     'type': 'start',
                     'player1_name': self.player1_name,
                     'player2_name': self.player2_name,
                 })
-                await self.game_start()
+                await self.start_pong_game()
 
         except Exception:
             await self.close()
 
+    async def init_connection(self):
+        self.user = self.scope['user']
+        query_string = parse_qs(self.scope['query_string'].strip().decode())
+        self.type = query_string.get('type', [None])[0]
+        type_id = query_string.get('type_id', [None])[0]
+        if self.type is None or type_id is None:
+            raise ValueError("type or type_id가 지정되지 않았습니다.")
+        self.game = self.get_game(type_id)
+        self.group_name = f'{self.type}_{type_id}'
+
+    async def get_game(self, type_id):
+        if self.type == 'local':
+            return Local(id=type_id)
+        if self.type == 'tournament':
+            return Tournament(id=type_id)
+        if self.type == 'remote':
+            return Remote(id=type_id)
+
+    async def start_remote_game(self):
+        if self.group_name not in self.remote_game:
+            self.remote_game[self.group_name] = []
+        self.remote_game[self.group_name].append(self.user)
+
+        if len(self.remote_game[self.group_name]) >= 2:
+            await self.set_players_name()
+            player1 = get_object_or_404(User, intra_id=self.player1_name)
+            player2 = get_object_or_404(User, intra_id=self.player2_name)
+            await self.channel_layer.group_send(self.group_name, {
+                'type': 'start',
+                'player1_name': self.player1_name,
+                'player1_image': player1.image,
+                'player2_name': self.player2_name,
+                'player2_image': player2.image
+            })
+            await self.start_pong_game()
+
+    async def set_players_name(self):
+        self.player1_name = self.game.player1_name
+        self.player2_name = self.game.player2_name
+
+    async def start(self, event):
+        try:
+            await self.send_json(event)
+        except Exception as e:
+            await self.send_json({'error': str(e)})
+
+    async def start_pong_game(self):
+        self.pong = Pong(self)
+        self.task = asyncio.create_task(self.pong.run())
+
     async def disconnect(self, code):
         try:
             if type == 'remote':
-                if self.pong.winner is None: #비정상 종료 처리
-                    await self.update_lose()
-                    await self.send_end_abnormal()
-                    if self.task is None:
-                        await self.update_win()
-                else: #정상 종료 처리
-                    if self.task:
-                        await self.update_result(self.pong.winner)
-
-                if self.user in self.remote_game[self.group_name]:
-                    self.remote_game[self.group_name].remove(self.user)
+                await self.disconnect_remote()
 
             #토너먼트, 로컬
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
             await self.cancel_task()
         except Exception as e:
             await self.send_json({'error': str(e)})
+
+    async def disconnect_remote(self):
+        if self.pong.winner is None:  # 비정상 종료 처리
+            await self.update_lose()
+            await self.send_end_abnormal()
+            if self.task is None:
+                await self.update_win()
+        else:  # 정상 종료 처리
+            if self.task:
+                await self.update_result(self.pong.winner)
+
+        if self.user in self.remote_game[self.group_name]:
+            self.remote_game[self.group_name].remove(self.user)
 
     @database_sync_to_async
     async def update_lose(self):
@@ -82,67 +132,11 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         self.user.game_count += 1
         self.user.save()
 
-
     @database_sync_to_async
     async def update_result(self):
         winner = get_object_or_404(User, intra_id=self.pong.winner)
 
-    async def init_connection(self):
-        self.user = self.scope['user']
-        query_string = parse_qs(self.scope['query_string'].strip().decode())
-        type = query_string.get('type', [None])[0]
-        type_id = query_string.get('type_id', [None])[0]
-        if type is None or type_id is None:
-            raise ValueError("type or type_id 가 지정되지 않았습니다.")
-        self.type = type
-        self.game = self.get_game(type, type_id)
-        self.group_name = 'type + type_id'
-
-    async def get_game(self, type, type_id):
-        if type == 'local':
-            return Local(id=type_id)
-        if type == 'tournament':
-            return Tournament(id=type_id)
-        if type == 'remote':
-            return Remote(id=type_id)
-
-    async def remote_game_start(self):
-        if self.group_name not in self.remote_game:
-            self.remote_game[self.group_name] = []
-        self.remote_game[self.group_name].append(self.user)
-
-        if len(self.remote_game[self.group_name]) >= 2:
-            await self.get_player_user()
-            player1 = get_object_or_404(User, intra_id=self.player1_name)
-            player2 = get_object_or_404(User, intra_id=self.player2_name)
-            await self.channel_layer.group_send(self.group_name, {
-                'type': 'start',
-                'player1_name': self.player1_name,
-                'player1_image': player1.image,
-                'player2_name': self.player2_name,
-                'player2_image': player2.image
-            })
-            await self.game_start()
-
-
-    async def get_player_user(self):
-        self.player1_name = self.game.player1_name
-        self.player2_name = self.game.player2_name
-
-    async def start(self, event):
-        try:
-            await self.send_json(event)
-        except KeyError as e:
-            await self.send({'error': f'{str(e)} is required'})
-        except Exception as e:
-            await self.send_json({'error': str(e)})
-
-    async def game_start(self):
-        self.pong = Pong(self)
-        self.task = asyncio.create_task(self.pong.run())
-
     async def send_end_abnormal(self):
-
         await self.channel_layer.group_send(self.group_name, {
             'type': 'end_abnormal',
             'winner': self.user.intra_id
@@ -160,7 +154,6 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
 
     def is_player1(self):
         return self.user.intra_id == self.player1_name
-
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
         try:
