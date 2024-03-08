22d68046c17b4a684bf59a214f53e902a242628b
yeeuniii <anna862700@gmail.com>
2024-03-08 19:43:25 +0900

    refactor/pong-together/backend#33: 상수 추가 및 1초에 몇 번 데이터 전송할지 조정

--- srcs/games/pong.py
+++ srcs/games/pong.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from games import constants
 from games.ball import Ball
 from games.paddle import Paddle
@@ -18,13 +20,14 @@ class Pong:
         self.player2 = Paddle(paddle2_x, paddle_y)
 
         ball_x = (constants.GAME_WIDTH - Ball.WIDTH) / 2
-        ball_y = (constants.GAME_HEIGHT - Ball.HEIGHT) / 2
+        ball_y = (constants.GAME_HEIGHT - Ball.HEIGHT) / 2 - 50
         self.ball = Ball(ball_x, ball_y, self.consumer.mode)
 
     async def run(self):
         try:
             while not self.finish():
                 await self.play_round()
+                await self.send_scores()
                 self.turn = (self.turn + 1) % 2
             self.set_winner()
             await self.send_game_finish()
@@ -41,6 +44,7 @@ class Pong:
             if self.player2.hit_ball(self.ball):
                 self.ball.bounce(self.player2)
             await self.send_game_info()
+            await asyncio.sleep(constants.SLEEP_TIME)
         self.update_score(status)
 
     def update_score(self, status):
@@ -51,18 +55,13 @@ class Pong:
             self.scores[index] += 1
 
     def finish(self):
-        return self.scores[constants.PLAYER1] == 10 \
-            or self.scores[constants.PLAYER2] == 10
+        return self.scores[constants.PLAYER1] == constants.END_POINT \
+            or self.scores[constants.PLAYER2] == constants.END_POINT
 
     def set_winner(self):
         self.winner = self.consumer.player1.intra_id
         if self.scores[constants.PLAYER2] == 10:
             self.winner = self.consumer.player2.intra_id
-
-    def update_positions(self, game):
-        self.player1.set_position(game.player1_y)
-        self.player2.set_position(game.player2_y)
-        self.ball.set_position(game.ball_x, game.ball_y)
 
     async def send_game_info(self):
         game_info = {
@@ -72,7 +71,7 @@ class Pong:
             'ball_x': self.ball.x,
             'ball_y': self.ball.y
         }
-        await self.consumer.channel_layer.group_send(self.consumer.GROUP_NAME, game_info)
+        await self.consumer.channel_layer.group_send(self.consumer.group_name, game_info)
 
     async def send_scores(self):
         data = {
@@ -84,7 +83,7 @@ class Pong:
 
     async def send_game_finish(self):
         data = {
-            'type': 'finish',
+            'type': 'end',
             'winner': self.winner
         }
         await self.consumer.channel_layer.group_send(self.consumer.group_name, data)
