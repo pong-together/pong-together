ae47307f6721b685ae3cd77b53e830cd32d4f381
wnswo8628 <gbjwein1234@naver.com>
2024-03-10 14:13:23 +0900

    feat/pong-together/frontend#72: LocalAPI 완성

--- src/pages/components/game/Game.js
+++ src/pages/components/game/Game.js
@@ -12,6 +12,16 @@ export default class extends Component {
 		} else {
 			http.checkToken();
 		}
+		// this.$state = {
+		// 	player1: '',
+		// 	player2: '',
+		// 	gameMode: window.localStorage.getItem('gameMode'),
+		// 	player1_result: '',
+		// 	player1_score: '',
+		// 	player2_result: '',
+		// 	player2_score: '',
+		// 	winner: '',
+		// }
 	}
 
 	template() {
@@ -36,7 +46,85 @@ export default class extends Component {
 		`;
 	}
 
+	// connectGameSocket() {
+	// 	const gameSocket = new WebSocket(
+	// 		`${SOCKET_URL}/ws/games/?token=${localStorage.getItem('accessToken')}&type=${localStorage.getItem('gameMode')}&type_id=${localStorage.getItem('local-id')}`,
+	// 	)
+		
+	// 	gameSocket.onopen = () => {
+	// 		this.addEvent('keypress', '.game-display', (e) => {
+	// 			if (e.key === 'w') {
+	// 				const message = {
+	// 					type: `${localStorage.getItem('gameMode')}`,
+	// 					send_player: `${this.$state.player1}`,
+	// 					button: "up",
+	// 				};
+
+	// 				gameSocket.send(JSON.stringify(message));
+	// 			}
+	// 			else if (e.key === 's'){
+	// 				const message = {
+	// 					type: `${localStorage.getItem('gameMode')}`,
+	// 					send_player: `${this.$state.player1}`,
+	// 					button: "down",
+	// 				};
+
+	// 				gameSocket.send(JSON.stringify(message));
+	// 			}
+	// 			else if (e.key === 'p') {
+	// 				const message = {
+	// 					type: `${localStorage.getItem('gameMode')}`,
+	// 					send_player: `${this.$state.player2}`,
+	// 					button: "up",
+	// 				};
+
+	// 				gameSocket.send(JSON.stringify(message));
+	// 			}
+	// 			else if (e.key === ';') {
+	// 				const message = {
+	// 					type: `${localStorage.getItem('gameMode')}`,
+	// 					send_player: `${this.$state.player2}`,
+	// 					button: "down",
+	// 				};
+	// 				gameSocket.send(JSON.stringify(message));
+	// 			}
+	// 		})
+	// 	}
+
+	// 	gameSocket.onclose = () => {
+	// 		console.log('gamesocket disconnect... Trying to reconnect...');
+	// 		setTimeout(() => this.connectGameSocket(), 1000);
+	// 	}
+
+	// 	gameSocket.onerror = function (e) {
+	// 		// console.log(e);
+	// 	}
+
+	// 	gameSocket.onmessage = (event) => {
+	// 		console.log(event.data);
+	// 		const data = JSON.parse(event.data);
+	// 		if (data.type && data.type === 'start') {
+	// 			this.setState({ player1: data.player1 });
+	// 			this.setState({ player2: data.player2 });
+	// 		}
+	// 		else if (data.type && data.type === 'end') {
+	// 			this.setState ({winner: data.winner});
+	// 			if (data.is_normal === false)
+	// 				gameSocket.close();
+	// 		}
+	// 		else if (data.type && data.type === 'get_game_info') {
+	// 			//전역으로 공좌표 관리하기?
+	// 			store.dispatch('ball_xChange', data.ball_x);
+	// 			console.log('ball_x = ' + store.state.ball_x);
+	// 			store.dispatch('ball_yChange', data.ball_y);
+	// 			store.dispatch('player1_yChange', data.player1_y);
+	// 			store.dispatch('player2_yChange', data.player2_y);
+	// 		}
+	// 	}
+	// }
+
 	mounted() {
 		new GameReady(document.querySelector('.game-display'));
+		// this.connectGameSocket();
 	}
 }
