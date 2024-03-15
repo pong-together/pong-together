20f2c753575f1de7ac8bf9fb2beee30488f4555f
ppparkta <pppppark2@gmail.com>
2024-03-15 20:33:53 +0900

    chore/pong-together/frontend#96: index.js 라우터 삭제

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import store from './store/index.js';
 import Router from './router/Router.js';
 
 new App(document.querySelector('#app'), store).init(document.querySelector('#app'));
-
-const $body = this.$target.querySelector('#app');
-new Router($body);
