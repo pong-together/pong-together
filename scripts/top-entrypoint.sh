393807afbad15413a79dfbd9d942c9e3334ae394
yeeuniii <anna862700@gmail.com>
2024-03-19 21:41:29 +0900

    build/pong-together/backend: asgi/wsgi 실행 pid1로 수정

--- scripts/top-entrypoint.sh
+++ scripts/top-entrypoint.sh
@@ -4,4 +4,4 @@ chmod +x manage.py
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata dummy_data.json
-daphne -b 0.0.0.0 -p 8000 pong_together.asgi:application
+exec daphne -b 0.0.0.0 -p 8000 pong_together.asgi:application
