2498f8cbe958a2e5c521a39330e52df33f0c1df1
vlssm <chrrvlssm@gmail.com>
2024-03-21 16:59:35 +0900

    fix/pong-together/frontend#110: 매칭 취소 문제 해결

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -24,17 +24,17 @@ function displayConnectionFailedModal(text) {
 }
 
 const displayCanceledMatchingModal = async (text, mainboxElement) => {
+	const modalHTML = `
+		<div class="modal-overlay">
+			<div class="modal-content">${text}</div>
+		</div>
+	`;
+
 	const sleep = async (ms) => {
 		await new Promise((resolve) => setTimeout(resolve, ms));
 	};
 
-	const overlayElement = document.createElement('div');
-	const contentElement = document.createElement('div');
-	mainboxElement.appendChild(overlayElement);
-	overlayElement.appendChild(contentElement);
-	overlayElement.className = 'modal-overlay';
-	contentElement.className = 'modal-content';
-	contentElement.textContent = text;
+	mainboxElement.innerHTML += modalHTML;
 	await sleep(3000);
 	const modalOverlay = document.querySelector('.modal-overlay');
 	modalOverlay.parentNode.removeChild(modalOverlay);
