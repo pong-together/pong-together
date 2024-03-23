e1ec56ffee7e0b3635262525d4aea870b68d08a5
ppparkta <pppppark2@gmail.com>
2024-03-23 14:08:57 +0900

    fix/pong-together/frontend#121: NotFound 버튼 버그 수정

--- src/pages/components/NotFound.js
+++ src/pages/components/NotFound.js
@@ -14,10 +14,11 @@ export default class NotFound extends Component {
 	setEvent() {
 		this.addEvent('click', '.not-found-btn', () => {
 			if (localStorage.getItem('accessToken'))
-			 	navigate("/select", true);
+				window.location.pathname = '/select';
 			else window.location.pathname = '/';
 		});
 	}
+
 	template() {
 		return `
 		<div class="body-wrapper">
