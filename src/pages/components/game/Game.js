f3affb86ea4a3394565f852618703fd88727ce1e
wnswo8628 <gbjwein1234@naver.com>
2024-03-24 20:12:17 +0900

    feat/pong-together/frontend#74: endendend

--- src/pages/components/game/Game.js
+++ src/pages/components/game/Game.js
@@ -325,6 +325,12 @@ export default class Game extends Component {
 		const popEvent = (e) => {
 			document.removeEventListener('keydown', this.event1);
 			document.removeEventListener('keyup', this.event2);
+			if (window.localStorage.getItem('gameMode') === 'local') {
+				window.localStorage.removeItem('local-id');
+			}
+			else if (window.localStorage.getItem('gameMode') === 'tournament'){
+				window.localStorage.removeItem('tournament-id');
+			} else window.localStorage.removeItem('remote-id');
 			clearInterval(this.time);
 			if (this.gameSocket.readyState === WebSocket.OPEN)
 				this.gameSocket.close();
