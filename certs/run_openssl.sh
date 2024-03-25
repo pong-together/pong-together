5eaece86562cd4452c8c40ebe1d4ae34b60908e8
ppparkta <pppppark2@gmail.com>
2024-03-25 13:07:50 +0900

    chore/pong-together/frontend#146: certs 파일 삭제

--- certs/run_openssl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ${PWD}/certs
-if [ ! -d "./nginx" ]; then
-    mkdir nginx
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout ./nginx/nginx.key \
-        -out ./nginx/nginx.crt \
-        -subj "/C=KO/ST=Seoul/L=Gaepo-dong/O=42Seoul/CN=localhost"
-fi
